9a6cf42c4197de2c1465d99c36aa88845ab3773c
tayloryounkins <tayloryounkins@10.192.109.76>
2019-09-23 00:57:30 -0400

    fully pairs everyone

---
+++ src/ListNode.java
@@ -0,0 +1,30 @@
+
+public class ListNode {
+	
+	Girl data;
+	ListNode next;
+	
+	public ListNode(Girl data, ListNode next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	public Girl getData() {
+		return data;
+	}
+
+	public void setData(Girl data) {
+		this.data = data;
+	}
+
+	public ListNode getNext() {
+		return next;
+	}
+
+	public void setNext(ListNode next) {
+		this.next = next;
+	}
+	
+	
+
+}
--- src/WittySistersPairing.java
+++ src/WittySistersPairing.java
@@ -3,137 +3,220 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-
+import java.util.Map.Entry;
 
 public class WittySistersPairing {
-	
+
 	private static String fileName = "scrubbed data.csv";
 	static ArrayList<String[]> data;
-	static Hashtable myTable = new Hashtable();
+	static HashMap myTable = new HashMap();
 	static ArrayList<Girl> girls;
 	static ArrayList<Girl> mentors = new ArrayList<Girl>();
 	static ArrayList<Girl> mentees = new ArrayList<Girl>();
 
 	public static void makeGirls() {
-		
+
 		/*
-		 * 1. sort the data based on major.
-		 * 2. collect the unsorted and sort+pair based on department. if necessary 
-		 * 3. pair based on mentee and mentor status.
-		 * print out all left overs for individual evaluation
+		 * 1. sort the data based on major. 2. collect the unsorted and sort+pair based
+		 * on department. if necessary 3. pair based on mentee and mentor status. print
+		 * out all left overs for individual evaluation
 		 * 
-		 * pair: Taylor Younkins(Cybersecurity)(Mentor)(tyounkin@purdue.edu)
-		 * Mia Stoffel(Cybersecurity)(Mentee)(stoffelm@purdue.edu) 
+		 * pair: Taylor Younkins(Cybersecurity)(Mentor)(tyounkin@purdue.edu) Mia
+		 * Stoffel(Cybersecurity)(Mentee)(stoffelm@purdue.edu)
 		 * 
 		 */
 		System.out.println("hi");
 		girls = new ArrayList<Girl>();
-		
-			for(String line[] : data) {
-				
-				for(int i = 0; i < line.length; i++) {
-					line[i] = trimLeadingTrailingSpaces(line[i]);
-				}
-			
-				String secondMajor = (line[3].equals("")) ? "None" : line[3];
-				String allergies = (line[6].equals("")) ? "None" : line[6];
-				
-				String hobbies = "";
-				for(int i = 9; i < line.length-5; i++) {
-					hobbies+=line[i];
-				}
-				
-				
-				//first name, last name, 1st major2, 2nd major3, year4 , email5, cell6, allergies7, mRole8, hobbies9, gain10
-				Girl g = new Girl(line[0], line[1], line[2], secondMajor, line[4], line[5], allergies, line[7], line[8], hobbies, line[line.length-5], line[line.length-1]);
-				
-				if(g.getmRole().equals("Mentor")) {
-					mentors.add(g);
-				} else {
-					mentees.add(g);
-				}
-				
-				//System.out.println(g.toString());
-				girls.add(g);
-				
+
+		for (String line[] : data) {
+
+			for (int i = 0; i < line.length; i++) {
+				line[i] = trimLeadingTrailingSpaces(line[i]);
 			}
-		
+
+			String secondMajor = (line[3].equals("")) ? "None" : line[3];
+			String allergies = (line[6].equals("")) ? "None" : line[6];
+
+			String hobbies = "";
+			for (int i = 9; i < line.length - 5; i++) {
+				hobbies += line[i];
+			}
+
+			// first name, last name, 1st major2, 2nd major3, year4 , email5, cell6,
+			// allergies7, mRole8, hobbies9, gain10
+			Girl g = new Girl(line[0], line[1], line[2], secondMajor, line[4], line[5], allergies, line[7], line[8],
+					hobbies, line[line.length - 5], line[line.length - 1]);
+
+			if (g.getmRole().equals("Mentor")) {
+				mentors.add(g);
+			} else {
+				mentees.add(g);
+			}
+
+			girls.add(g);
+
+		}
+
 	}
 
 	public static String trimLeadingTrailingSpaces(String str) {
-		
-		if(str.isEmpty()) {
+
+		if (str.isEmpty()) {
 			return "";
 		}
-		
-		if(str.substring(0).equals(" "))
+
+		if (str.substring(0).equals(" "))
 			str = str.substring(1, str.length());
-		
-		if(str.substring(str.length()-1).equals(" "))
-			str = str.substring(0, str.length()-1);
-			
-		
+
+		if (str.substring(str.length() - 1).equals(" "))
+			str = str.substring(0, str.length() - 1);
+
 		return str;
 	}
-	
+
 	public static void pair() {
-		
+
 		ArrayList<Girl> unpaired = new ArrayList<Girl>();
-		
-		for(int i = 0; i < mentees.size(); i++) {
-			
-			for(int j = 0; j < mentors.size(); j++) {
-				
+		ArrayList<Girl> totalPaired = new ArrayList<Girl>();
+
+		for (int i = 0; i < mentees.size(); i++) {
+
+			for (int j = 0; j < mentors.size(); j++) {
+
 				Girl mentor = mentors.get(j);
 				Girl mentee = mentees.get(i);
-
 				
-				if(mentor.getMajor().equalsIgnoreCase(mentee.getMajor()) || mentor.getMajor().equalsIgnoreCase(mentee.getSecondMajor())){
-					
-					 if(myTable.containsKey(mentor) == false && myTable.containsValue(mentee) == false) {
-						System.out.println("I put " + mentor.getFirstName() + " with " +mentee.getFirstName());
-						myTable.put(mentor, mentee);
-					} 
-					
-				} else if(mentor.getDepartment().equalsIgnoreCase(mentee.getDepartment())){
-					
-					if(myTable.containsKey(mentor) == false && myTable.containsValue(mentee) == false) {
-						System.out.println("I put " + mentor.getFirstName() + " with " +mentee.getFirstName());
-						myTable.put(mentor, mentee);
+				ArrayList<Girl> menteesList = new ArrayList<Girl>();
+				menteesList.add(mentee);
+
+				//if majors match
+				if (mentor.getMajor().equalsIgnoreCase(mentee.getMajor()) || mentor.getMajor().equalsIgnoreCase(mentee.getSecondMajor())) {
+					//if they both dont exist
+					if (myTable.containsKey(mentor) == false && myTable.containsValue(menteesList) == false) {
+						
+//						System.out.println("Mytable doesnt contain this mentor or this mentee. I put " + mentor.getFirstName() + " with " + mentee.getFirstName());
+
+						totalPaired.add(mentor);
+						totalPaired.add(mentee);
+						myTable.put(mentor, menteesList);
 					}
+				//if departments match
+				} else if (mentor.getDepartment().equalsIgnoreCase(mentee.getDepartment())) {
 					
-				} 
-				
+					//if they both dont exist
+					if (myTable.containsKey(mentor) == false && myTable.containsValue(menteesList) == false) {
+//						System.out.println("\nDepartments: I put " + mentor.getFirstName() + " with " + mentee.getFirstName() + mentee.getLastName());
+						
+						totalPaired.add(mentor);
+						totalPaired.add(mentee);
+						myTable.put(mentor, menteesList);
+					}
 
+				}
+			}
+
+		} //end first round pairing
+		
+		//2nd round pairing
+		
+		
+		for (Girl g : girls) {
+
+			if (!totalPaired.contains(g)) {
+//				System.out.println(g.getFirstName() + " is unpaired, major: " + g.getMajor() + " role: " + g.getmRole());
+			}
+
+		}
+		
+		System.out.println("2ND ROUND -- PAIR MATCHING MENTEES TO MENTORS---------------------------------------------------------");
+		
+		for (int i = 0; i < mentees.size(); i++) {
+
+			for (int j = 0; j < mentors.size(); j++) {
+
+				Girl mentor = mentors.get(j);
+				Girl mentee = mentees.get(i);
+				
+				ArrayList<Girl> menteesList = new ArrayList<Girl>();
+				menteesList.add(mentee);
+				
+				//if majors match
+				if (mentor.getMajor().equalsIgnoreCase(mentee.getMajor()) || mentor.getMajor().equalsIgnoreCase(mentee.getSecondMajor())) {
+					//if they both don't exist
+					
+					ArrayList<Girl> thisMentorsList = (ArrayList<Girl>)myTable.get(mentor);
+					
+					
+					if (myTable.containsKey(mentor) == true && !totalPaired.contains(mentee) && thisMentorsList.size() <2 ) {
+						
+//						System.out.println("Mytable contains this mentor but not this mentee. I put " + mentor.getFirstName() + " with " + mentee.getFirstName());
+						thisMentorsList.add(mentee);
+//						System.out.println("this mentor's list is: " + thisMentorsList.size());
+
+						totalPaired.add(mentee);
+						myTable.put(mentor, thisMentorsList);
+					}
+				//if departments match
+				} else if (mentor.getDepartment().equalsIgnoreCase(mentee.getDepartment())) {
+					
+					ArrayList<Girl> thisMentorsList = (ArrayList<Girl>)myTable.get(mentor);
+					
+					
+					//if they both dont exist
+					if (myTable.containsKey(mentor) == true && !totalPaired.contains(mentee)  && thisMentorsList.size() <2) {
+//						System.out.println("Departments: I put " + mentor.getFirstName() + " with " + mentee.getFirstName() + mentee.getLastName());
+						thisMentorsList.add(mentee);
+//						System.out.println("this mentor's list is: " + thisMentorsList.size());
+
+						totalPaired.add(mentee);
+						myTable.put(mentor, thisMentorsList);
+					}
+
+				}
+			}
+
+		} //end 2nd round pairing
+
+
+		for (Girl g : girls) {
+
+			if (!totalPaired.contains(g)) {
+//				System.out.println(g.getFirstName() + " is unpaired, major: " + g.getMajor() + " role: " + g.getmRole());
+			}
+
+		}
+		
+		Set<Entry> entries = myTable.entrySet();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(Entry entry : entries) {
+			Girl key = (Girl)entry.getKey();
+			ArrayList<Girl> values = (ArrayList<Girl>)entry.getValue();
+			
+			String s = key.getFirstName() + " major/department:   "  + key.getMajor() + "/" + key.getDepartment();
+			System.out.println(s);
+			sb.append(s);
+			
+			
+			for(Girl value : values) {
+				
+				String mt = "   " + value.getFirstName() +"   " +value.getMajor() + "/" + value.getDepartment();
+				System.out.println(mt);
+				sb.append(mt);
 				
 			}
 			
 		}
-		
-		for(Girl g : girls) {
-			
-			if(!myTable.containsKey(g) && !myTable.containsValue(g)) {
-				unpaired.add(g);
-				System.out.println(g.getFirstName() + " is unpaired, major: " + g.getMajor() + " role: " + g.getmRole());
-			}
-			
-		}
-		
-		
-		
-		System.out.println(myTable.toString());
-		
-		
-		
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		data = WittySistersEmail.getData();
 		makeGirls();
 		pair();
-		
+
 	}
 
 }
